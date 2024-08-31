1a2f28863b0646fbf0b5f829a2de1c42ebe7e681
jobehay <jobehaya@gmail.com>
2024-08-31 14:07:19 +0300

    remove tokens

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker login -u jobehay -p Devops2023
-docker pull jobehay/final-python:45
-sudo docker run --name finpy -it -d -p 5000:5000 jobehay/final-python:45
+docker login -u ayajo12 -p Devops2023
+docker pull ayajo12/final-python:45
+sudo docker run --name finpy -it -d -p 5000:5000 ayajo12/final-python:45
